fcf3943ac67013a3feb0dcd058d3e81a56ddda74
foliedujour <kimonasbarlas@gmail.com>
2023-12-11 19:48:00 +0200

    Main updated

--- src/gr/aueb/cf/Main.java
+++ src/gr/aueb/cf/Main.java
@@ -3,6 +3,6 @@ package gr.aueb.cf;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello CF!!!");
+        System.out.println("Hello Coding Factory!!!!!!!");
     }
 }
